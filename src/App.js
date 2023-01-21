bbeb65b916e8a145188028cec1daf1b1f70ff28b
nleitefaria <nleitefaria@gmail.com>
2023-01-21 18:55:56 +0000

    Clean

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
 import About from "./About";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
