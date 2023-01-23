154e0e8f94673dd6ca3c28989234d869bed42215
nleitefaria <nleitefaria@gmail.com>
2023-01-23 11:25:40 +0000

    feature2 : Added fetch

--- src/Artworks.js
+++ src/Artworks.js
@@ -1,4 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
 const Artworks = () => {
+
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('https://api.artic.edu/api/v1/artworks?limit=2')
+             .then((response) => response.json())
+             .then((data) => {
+                console.log(data);
+                setPosts(data);
+             })
+             .catch((err) => {
+                console.log(err.message);
+             });
+       }, []);
+
+
   return(
     <div className="container-fluid">
         <h1>Artworks</h1>
